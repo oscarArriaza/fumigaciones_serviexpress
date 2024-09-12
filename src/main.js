f02af02ea4a06e0df905715d8119a4c8bc9a9e63
oscarArriaza <arriaza22598@vk.com>
2024-09-12 08:16:35 +0200

    oscar

--- src/main.js
+++
@@ -1,4 +0,0 @@
-import { createApp } from 'vue'
-import Fumigaciones_serviexpress from './components/Fumigaciones_serviexpress.vue'
-
-createApp(Fumigaciones_serviexpress).mount('#fumigaciones_serviexpress')
